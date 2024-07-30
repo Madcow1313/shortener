0d620be75b59dba6f3f1cff57e7e323da5daee4c
Madcow1313 <safbul@gmail.com>
2024-07-30 21:53:39 +0300

    :(

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -24,7 +24,7 @@ const (
 	defaultHost        = "localhost:8080"
 	defaultBaseURL     = ""
 	defaultURLstorage  = ""
-	defaultDatabaseDSN = "user=postgres password=postgres host=localhost"
+	defaultDatabaseDSN = ""
 	defaultSecretKey   = "cookieinthejar"
 )
 
